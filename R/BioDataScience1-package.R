43eb02231ce24da023775710c16062a79596b64a
Philippe Grosjean <phgrosjean@sciviews.org>
2024-08-28 16:40:12 +0200

    Preparation for academic year 2024-2025

--- R/BioDataScience1-package.R
+++ R/BioDataScience1-package.R
@@ -2,14 +2,17 @@
 #'
 #' Run the tutorials using[run()].
 #'
-#' @docType package
 #' @name BioDataScience1-package
+"_PACKAGE"
 #'
 #' @importFrom learnitdown run learnitdownLearnrBanner learnitdownLearnrServer
 #' @importFrom BioDataScience config sign_in sign_out
 #' @importFrom shiny dialogViewer observeEvent selectInput stopApp runGadget
 #' @importFrom miniUI gadgetTitleBar miniContentPanel miniPage miniTitleBarButton miniTitleBarCancelButton
 #' @importFrom rstudioapi showDialog
+#' @importFrom BioDataScience config sign_in sign_out
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
-
-#@importFrom BioDataScience config sign_in sign_out run run_app update_pkg
